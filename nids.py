980fae06cf2e77889aa8634d629376889ee37ed0
Hava Kantrowitz <hava@hava.hava>
2021-02-07 23:59:30 -0500

    sig finding works

--- nids.py
+++ nids.py
@@ -7,10 +7,15 @@ import binascii
 import socket
 import scapy.all as scapy
 import copy
+import base64
 
-AT1 = "iOwIVM5mu1Qc5QsUR11iFhgc3aB//ITN8hivvCrfSn8=".encode('utf-8')
-AT2 = "GtV6G6z3BWTvxqd0Eh4zD81UZ1sDtOWQtF1/9kGRBTc=".encode('utf-8')
-AT3 = "8DJNxln1Gv65fYpjwat2fYkRTCz023YUT1yKZWCfFWI=".encode('utf-8')
+AT1 = "iOwIVM5mu1Qc5QsUR11iFhgc3aB//ITN8hivvCrfSn8="
+AT2 = "GtV6G6z3BWTvxqd0Eh4zD81UZlsDtOWQtF1/9kGRBTc="
+AT3 = "8DJNxln1Gv65fYpjwat2fYkRTCz023YUT1yKZWCfFWI="
+
+AT1 = base64.b64decode(AT1)
+AT2 = base64.b64decode(AT2)
+AT3 = base64.b64decode(AT3)
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -19,24 +24,13 @@ def parse_args():
     pcap_file = args.pcap
     return pcap_file
 
-#THIS CODE IS ADAPTED FROM SURAJ SINGH'S ARTICLE ON PYTHON CODES TO CALCULATE IPV4 CHECKSUMS FROM BITFORESTINFO.COM
-def calc_cksum(msg):
-    s = 0
-    for i in range(0, len(msg), 2):
-        if (i+1) < len(msg): 
-            a = ord(msg[i])
-            b = ord(msg[i+1])
-            s = s + (a + (b << 8))
-        elif (i + 1) == len(msg):
-            s += ord(msg[i])
-        else:
-            print("Error!")
-    s = s + (s >> 16)
-    s = ~s & 0xffff
-    return s
-
-def inet_to_str(inet):
-    return socket.inet_ntop(socket.AF_INET, inet)
+def find_attack(data):
+    if AT1 in data:
+        print("attack 1 found")
+    if AT2 in data:
+        print("attack 2 found")
+    if AT3 in data:
+        print("attack 3 found") 
 
 def parse_pcap(pcap_file): 
     packets = scapy.PcapReader(pcap_file)
@@ -54,68 +48,23 @@ def parse_pcap(pcap_file):
                     frags[idv].append(pkt)
                 else: 
                     frags[idv] = [pkt] 
-    print(len(frags)) 
     for key in frags.keys(): 
         off_list = {}
         for val in frags[key]: 
             if val.haslayer("Raw"):
                 off = getattr(val["IP"], "frag")
-                data = val["Raw"]
+                data = val["Raw"].load
                 off_list[off] = data
-        highest_val = sorted(off_list)[-1]
-        prev_i = 0
-        data = ""
-        for i in range(0, highest_val+1):
-            full_data = off_list[i]
-            data = full_data[prev_i:i]
-        print(data)
+        if len(off_list) == 1:
+            find_attack(off_list[0])
+        else:
+            highest_val = sorted(off_list)[-1]
+            data = b''
+            for i in range(1, highest_val+1):
+                data = data + off_list[i]
+            print(data)
+            find_attack(data)
 
-
-def parse_pcap2(pcap_file):
-    f = open(pcap_file, 'rb')
-    pcap = dpkt.pcap.Reader(f)  
-    packets = {}
-    for ts, buf in pcap: 
-        eth = dpkt.ethernet.Ethernet(buf)
-        ip = eth.data
-        print(type(ip))
-        calc_cksum(ip)
-        if ip.p == 6: 
-            print(repr(eth))
-            df = bool(ip.off & dpkt.ip.IP_DF)
-            mf = bool(ip.off & dpkt.ip.IP_MF)
-            offset = ip.off & dpkt.ip.IP_OFFMASK
-            if ip.id in packets.keys():
-                packets[ip.id].append(ip)
-            else: 
-                packets[ip.id] = []
-                packets[ip.id].append(ip)
-
-    del packets[14736]
-    for key in packets.keys():
-        #print(key)
-        off_dict = {}
-        data = ""
-        for val in packets[key]:
-            offset = val.off & dpkt.ip.IP_OFFMASK
-            data = val.data
-            off_dict[offset] = data
-        highest_val = sorted(off_dict)[-1]
-        prev_i = 0
-        for i in range(0, highest_val+1):
-            full_data = off_dict[i] 
-            data = data + full_data[prev_i:i]
-        #print(data) 
-        if AT1 in data: 
-            print("attack 1 found!")
-        if AT2 in data:
-            print("attack 2 found!")
-        if AT3 in data:
-            print("attack 3 found!") 
-
-    #print("AT1 is ", AT1)
-                
-            #print('IP: %s -> %s   (id=%d ttl=%d DF=%d MF=%d offset=%d)' % (inet_to_str(ip.src), inet_to_str(ip.dst), ip.id, ip.ttl, df, mf, offset))
 
 def main():
     pcap_file = parse_args()
